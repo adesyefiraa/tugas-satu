687711f94f57e06b9feab2ede2d067458652c41b
adesyefiraa <102138207+adesyefiraa@users.noreply.github.com>
2022-03-22 13:49:20 +0700

    Add files via upload

---
+++ TugasSatu.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas.satu;
+
+/**
+ *
+ * @author Ade Syefira Rizky
+ * date 22/03/2022/12:57
+ */
+public class TugasSatu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.print("hallo dunia");
+    }
+    
+}
